035fd96c5d7cfb1abe142743313e1afcfb48eda5
sam sepio <samsepio66@gmail.com>
2020-02-10 22:29:33 +0000

    utilisacion de multipoles condicionales

--- script5.sh
+++ script5.sh
@@ -1,2 +1,17 @@
-echo "nombre del archivo: "
-read archivo;
+echo "eliga su opcion C=crear E=eliminar: "
+read opcion;
+if [[ $opcion = *c* ]]
+then
+	echo "nombre del archivo a crear: "
+	read archivo;
+	touch $archivo;
+	echo "archivo creado correctamente"
+elif [[ $opcion = *e* ]]
+then
+	echo "nombre de el archivo a eliminar: "
+	read delete;
+	sudo rm -r $delete;
+	echo "archivo eliminado correctamente"
+else
+	echo "opcion no encontrada"
+fi
